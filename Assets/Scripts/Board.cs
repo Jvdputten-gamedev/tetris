44214cbaef32426c4069c7d631bb57029c1b5e25
Joost <jvdputten@thetavision.nl>
2023-09-05 16:46:12 +0200

    ability to spawn tetrominoes

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
-    public TetrominoData[] tetrominos;
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece {get; private set; }
+    public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
+
+    private void Awake()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+        activePiece = GetComponentInChildren<Piece>();
+
+        for (int i = 0; i < tetrominoes.Length; i++) {
+            tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, tetrominoes.Length);
+        Debug.Log(random);
+        TetrominoData data = tetrominoes[random];
+
+        activePiece.Initialize(this, spawnPosition, data);
+        Set(activePiece);
+    }
+
+    public void Set(Piece piece){
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -15,9 +15,9 @@ public enum Tetromino{
 public struct TetrominoData{
     public Tetromino tetromino;
     public Tile tile;
-    public Vector2Int[] cells {get; private set;}
+    public Vector2Int[] cells { get; private set; }
 
     public void Initialize(){
-        this.cells = Data.Cells[this.tetromino];
+        cells = Data.Cells[tetromino];
     }
 }
